1bd38e9519657523e4a6dd32aecef908cc1a65ec
bdaf <kamilkorenkiewiczpw@gmail.com>
2022-01-04 09:07:35 +0100

    initial backend commit

---
+++ backend/src/test/java/com/bdaf/weapon_shop/WeaponShopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bdaf.weapon_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeaponShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
