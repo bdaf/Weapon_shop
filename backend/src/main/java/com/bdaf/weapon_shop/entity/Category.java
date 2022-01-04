8a68c8c8c1058ae743e70ae70ca9cf7b7cbeb87b
bdaf <kamilkorenkiewiczpw@gmail.com>
2022-01-04 09:37:22 +0100

    Entities category and discount completed

--- backend/src/main/java/com/bdaf/weapon_shop/entity/Category.java
+++ backend/src/main/java/com/bdaf/weapon_shop/entity/Category.java
@@ -1,4 +1,47 @@
 package com.bdaf.weapon_shop.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ws_category")
 public class Category {
+    public static final String OTHER = "Inny";
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "category_sequence")
+    @SequenceGenerator(name = "category_sequence", sequenceName = "category_sequence", allocationSize = 1)
+    @Column(name = "category_id", nullable = false)
+    private Long categoryId;
+
+    @Column(name = "name", length = 63, unique = true)
+    private String name = OTHER;
+
+    @ManyToMany(cascade = CascadeType.ALL, fetch = FetchType.EAGER)
+    @JoinTable(name = "IS_category_discount_map",
+            joinColumns = @JoinColumn(
+                    name = "category_id",
+                    referencedColumnName = "category_id"
+            ),
+            inverseJoinColumns = @JoinColumn(
+                    name = "discount_id",
+                    referencedColumnName = "discount_id"
+            )
+    )
+
+    private List<Discount> discounts;
+
+    public void addDiscount(Discount aDiscount){
+        if(discounts == null) discounts = new ArrayList<>();
+        discounts.add(aDiscount);
+    }
+
+
 }
--- backend/src/main/java/com/bdaf/weapon_shop/entity/Discount.java
+++ backend/src/main/java/com/bdaf/weapon_shop/entity/Discount.java
@@ -1,4 +1,31 @@
 package com.bdaf.weapon_shop.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.sql.Date;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "ws_discount")
 public class Discount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "discount_sequence")
+    @SequenceGenerator(name = "discount_sequence", sequenceName = "discount_sequence", allocationSize = 1)
+    @Column(name = "discount_id", nullable = false)
+    private Long discountId;
+
+    @Column(name = "percent", nullable = false, scale = 2, precision = 3)
+    private Float percent;
+
+    @Column(name = "from_date", nullable = false)
+    private Date fromDate;
+
+    @Column(name = "to_date", nullable = false)
+    private Date toDate;
 }
