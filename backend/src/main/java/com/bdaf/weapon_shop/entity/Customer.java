d85be18dc82970e822005277e94e1790fc4f703d
bdaf <kamilkorenkiewiczpw@gmail.com>
2022-01-04 09:24:59 +0100

    Entity customer completed

--- backend/src/main/java/com/bdaf/weapon_shop/entity/Customer.java
+++ backend/src/main/java/com/bdaf/weapon_shop/entity/Customer.java
@@ -1,4 +1,46 @@
 package com.bdaf.weapon_shop.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name="ws_customer")
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "customer_sequence")
+    @SequenceGenerator(name = "customer_sequence", sequenceName = "customer_sequence", allocationSize = 1)
+    @Column(name = "customer_id", nullable = false)
+    private Long customerId;
+
+    @Column(name = "name", length = 31, nullable = false)
+    private String name;
+
+    @Column(name = "surname", length = 31, nullable = false)
+    private String surname;
+
+    @Column(name = "email", length = 31, nullable = false)
+    private String email;
+
+    @Column(name = "phone_number", length = 15, nullable = false)
+    private String phoneNumber;
+
+    @Column(name = "street", length = 63, nullable = false)
+    private String street;
+
+    @Column(name = "house_number", length = 7, nullable = false)
+    private String houseNumber;
+
+    @Column(name = "city", length = 31, nullable = false)
+    private String city;
+
+    @Column(name = "postcode", length = 15, nullable = false)
+    private String postcode;
+
+    @Column(name = "country", length = 15, nullable = false)
+    private String country;
+
+    @Column(name = "nip", length = 15)
+    private String nip;
 }
