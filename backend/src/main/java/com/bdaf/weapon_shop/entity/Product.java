fa5f4fbf02d92e0abff695e171bd041757e97661
bdaf <kamilkorenkiewiczpw@gmail.com>
2022-01-04 09:45:50 +0100

    Entity product completed

--- backend/src/main/java/com/bdaf/weapon_shop/entity/Product.java
+++ backend/src/main/java/com/bdaf/weapon_shop/entity/Product.java
@@ -1,4 +1,48 @@
 package com.bdaf.weapon_shop.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+@Table(name = "ws_product")
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "product_sequence")
+    @SequenceGenerator(name = "product_sequence", sequenceName = "product_sequence", allocationSize = 1)
+    @Column(name = "product_id", nullable = false)
+    private Long productId;
+
+    @Column(name = "name", nullable = false, length = 63)
+    private String name;
+
+    @Column(name = "description", length = 511)
+    private String description = "Here should be description of product.";
+
+    @Column(name = "price", nullable = false)
+    private Double price;
+
+    @Column(name = "amount", nullable = false)
+    private int amount = 1;
+
+    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @JoinColumn(name = "producer_id", referencedColumnName = "producer_id")
+    private Producer producer;
+
+    @ManyToOne(cascade = CascadeType.MERGE, fetch = FetchType.EAGER)
+    @JoinColumn(name = "category_id", referencedColumnName = "category_id", nullable = false)
+    private Category category;
+
+    @Column(name = "photo_url")
+    private String photoUrl;
+
+    @Column(name = "for_sale", nullable = false)
+    private boolean forSale = true;
 }
