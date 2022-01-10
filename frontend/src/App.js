89294aa18a1b0568aa8d70b3d5df6689d5fd8447
bdaf <kamilkorenkiewiczpw@gmail.com>
2022-01-11 00:58:02 +0100

    Reducing unnecessary code

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap";
 
 import CustomNavbar from './Components/Navbar/CustomNavbar';
 
 function App() {
   return (
     <div className="App">
-      <CustomNavbar></CustomNavbar>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <CustomNavbar />
     </div>
   );
 }
