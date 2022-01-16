a8d15c4ad71a0805a4f417352fe0f1992451a76f
PaulinaTymoszuk <p.tymoszuk@op.pl>
2022-01-16 20:10:24 +0100

    Add a form and displaying the product

--- frontend/src/Pages/Products.js
+++ frontend/src/Pages/Products.js
@@ -1,57 +1,57 @@
-//import NewCategoryForm from "../Components/Category/NewCategoryForm";
+import NewProductForm from "../Components/Product/NewProductForm";
 import { useState, useEffect } from "react";
 import ProductsList from "../Components/Product/ProductsList";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:80/api/categories",
+    baseURL: "http://localhost:80/api/products",
 });
 
 function Products(props) {
-  async function addCategoryHandler(categoryData) {
+    async function addProductHandler(productData) {
     // http POST method
-    // const res = api
-    //   .post("/", { name: categoryData.name })
-    //   .then(function (response) {
-    //     // get result data from http method
-    //     const resultData = response.data;
-    //     // check if there isn't one with same name
-    //     const theSameCategory = loadedCategories.find(
-    //       (cat) => cat.name == resultData.name
-    //     );
-    //     // adding locally category to site if condition above
-    //     if (theSameCategory == null) {
-    //       setLoadedCategories((prev) => [...prev, resultData]);
-    //     }
-    //   });
-  }
+        const res = api
+            .post("/", { name: productData.name })
+            .then(function (response) {
+                // get result data from http method
+                const resultData = response.data;
+                // check if there isn't one with same name
+                const theSameProduct = loadedProducts.find(
+                    (cat) => cat.name == resultData.name
+                );
+                // adding locally product to site if condition above
+                if (theSameProduct == null) {
+                    setLoadedProducts((prev) => [...prev, resultData]);
+                }
+            });
+    }
 
-//   const [isLoading, setIsLoading] = useState(true);
-//   const [loadedCategories, setLoadedCategories] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+    const [loadedProducts, setLoadedProducts] = useState([]);
 
-//   useEffect(() => {
-//     api.get("/").then(function (response) {
-//       setIsLoading(false);
-//       setLoadedCategories(response.data);
-//     });
-//   }, []);
+    useEffect(() => {
+        api.get("/").then(function (response) {
+            setIsLoading(false);
+            setLoadedProducts(response.data);
+        });
+    }, []);
 
-//   if (isLoading) {
-//     return (
-//       <section>
-//         <p>Loading...</p>
-//       </section>
-//     );
-//   }
+    if (isLoading) {
+        return (
+            <section>
+                <p>Loading...</p>
+            </section>
+        );
+    }
 
-  return (
-    <div>
-      <h1>Products</h1>
-      <h1>Add new product:</h1>
-      {/* <NewProductForm onAddProduct={addProductHandler} />
-      <ProductsList products={loadedProducts} /> */}
-    </div>
-  );
+    return (
+        <div>
+            <h1>Products</h1>
+            <h1>Add new product:</h1>
+            <NewProductForm onAddProduct={addProductHandler} />
+            <ProductsList products={loadedProducts} />
+        </div>
+    );
 }
 
 export default Products;
