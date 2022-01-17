13cb8eb8bfe2200efb217367a235473144438954
bdaf <kamilkorenkiewiczpw@gmail.com>
2022-01-17 09:33:58 +0100

    Corrected route for main page (products)

--- frontend/src/Components/layout/CustomNavbar.js
+++ frontend/src/Components/layout/CustomNavbar.js
@@ -1,23 +1,39 @@
-import { Navbar, Container, Nav} from 'react-bootstrap';
+import { Navbar, Container, Nav } from "react-bootstrap";
 import { MDBIcon } from "mdb-react-ui-kit";
-import { Link } from 'react-router-dom';
-import styles from './CustomNavbar.module.css';
+import { Link } from "react-router-dom";
+import styles from "./CustomNavbar.module.css";
 
 function CustomNavbar() {
   return (
     <Navbar bg="dark" variant="dark" className={styles.customNavbar}>
-    <Container>
-      <Navbar.Brand href="#home">
-      <MDBIcon icon="hammer" style={{"marginRight": "10px"}}
-      />{' '}
-      Weapon shop
-      </Navbar.Brand>
-              <Nav.Link><Link to='products' class="link-light">Products</Link></Nav.Link>
-              <Nav.Link><Link to='orders' class="link-light">Orders</Link></Nav.Link>
-              <Nav.Link><Link to='categories' class="link-light">Category (OFA)</Link></Nav.Link>
-              <Nav.Link><Link to='discounts' class="link-light">Discounts (OFA)</Link></Nav.Link>
-    </Container>
-  </Navbar>
+      <Container>
+        <Navbar.Brand>
+          <Link to="products" class="link-light">
+            <MDBIcon icon="hammer" style={{ marginRight: "10px" }} /> Weaponshop
+          </Link>
+        </Navbar.Brand>
+        <Nav.Link>
+          <Link to="products" class="link-light">
+            Products
+          </Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="orders" class="link-light">
+            Orders
+          </Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="categories" class="link-light">
+            Category (OFA)
+          </Link>
+        </Nav.Link>
+        <Nav.Link>
+          <Link to="discounts" class="link-light">
+            Discounts (OFA)
+          </Link>
+        </Nav.Link>
+      </Container>
+    </Navbar>
   );
 }
 
